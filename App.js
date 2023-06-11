9d04658b0e3727a42e669d459dce44fbb70eff9f
mrohovets <mrohovets@gmail.com>
2023-06-11 22:57:46 +0300

    2:13:51

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Navbar } from "./src/Navbar";
+import { AddTodo } from "./src/AddTodo";
+import { Todo } from "./src/Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    const newTodo = {
+      id: Date.now().toString(),
+      title,
+    };
+    setTodos((prevTodos) => {
+      return [...prevTodos, newTodo];
+    });
+  };
   return (
-    <View style={styles.container}>
-      <Navbar />
+    <View>
+      <Navbar title="TodoApp" />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <View>
+          {todos.map((todo) => {
+            return <Todo todo={todo} key={todo.id} />;
+          })}
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
 });
